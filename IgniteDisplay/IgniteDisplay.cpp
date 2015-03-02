a10697aeb1d973fb78580442ff0aa84d6d7800cd
oldrake00 <riki.amemiya.ryoyo@gmailcom>
2015-03-02 18:58:46 +0900

    コメント修正した

--- IgniteDisplay/IgniteDisplay.cpp
+++ IgniteDisplay/IgniteDisplay.cpp
@@ -11,6 +11,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	HWND dummyWindowHandler;
 
 	dummyWindowHandler = GetForegroundWindow();
+
+	
 	PostMessage(dummyWindowHandler, WM_SYSCOMMAND, SC_MONITORPOWER, -1);
 	
 	return 0;
