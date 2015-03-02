8a6bdcab07d371f26a782d9a61ddbea71fb1d7dc
oldrake <riki.amemiya.ryoyo@gmailcom>
2015-03-02 16:27:22 +0900

    Initial commit

---
+++ IgniteDisplay/IgniteDisplay.cpp
@@ -0,0 +1,18 @@
+// IgniteDisplay.cpp
+//
+
+#include "stdafx.h"
+#include "windows.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	HWND dummyWindowHandler;
+
+	dummyWindowHandler = GetForegroundWindow();
+	PostMessage(dummyWindowHandler, WM_SYSCOMMAND, SC_MONITORPOWER, -1);
+	
+	return 0;
+}
+
